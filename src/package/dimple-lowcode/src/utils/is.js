41fec3ca994ec56d536a4a2dbbb019cb06d53e69
dimple-smile <1696819016@qq.com>
2022-08-18 14:24:22 +0800

    feat: 修改工具函数中http地址的校验为简单校验

--- src/package/dimple-lowcode/src/utils/is.js
+++ src/package/dimple-lowcode/src/utils/is.js
@@ -1,4 +1,4 @@
-const http = (o) => /^((https?|ftp|file):\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(o)
+const http = (o) => /^https?.*/.test(o)
 
 const is = { http }
 export { is }
